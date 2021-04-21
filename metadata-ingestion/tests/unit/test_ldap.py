c619a74c124443f489e53bb993448493795116fa
Adriaan Slechten <adriaan.slechten@gmail.com>
2021-04-21 14:19:15 +0200

    feat(ingest): Updating LDAP ingestion w. groups.

---
+++ metadata-ingestion/tests/unit/test_ldap.py
@@ -0,0 +1,43 @@
+from src.datahub.ingestion.source.ldap import strip_ldap_info, parse_from_attrs
+import pytest
+
+
+def test_strip_ldap_info():
+    assert (
+        strip_ldap_info(
+            b"uid=firstname.surname,ou=People,dc=internal,dc=machines"
+        )
+        == "firstname.surname"
+    )
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (
+            {
+                "admins": [
+                    b"uid=A.B,ou=People,dc=internal,dc=machines",
+                    b"uid=C.D,ou=People,dc=internal,dc=machines",
+                ]
+            },
+            ["urn:li:corpuser:A.B", "urn:li:corpuser:C.D"],
+        ),
+        (
+            {
+                "not_admins": [
+                    b"doesntmatter",
+                ]
+            },
+            [],
+        ),
+    ],
+)
+def test_parse_from_attrs(input, expected):
+    assert (
+        parse_from_attrs(
+            input,
+            "admins",
+        )
+        == expected
+    )
